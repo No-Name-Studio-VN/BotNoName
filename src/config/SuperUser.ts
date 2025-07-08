f6ded9f18e4ecee75d71b60b7219796577d2b790
Le Ngo Duc Manh <myt@nnsvn.me>
2025-07-08 10:31:02 +0700

    Update SuperUser.ts

--- src/config/SuperUser.ts
+++ src/config/SuperUser.ts
@@ -1,2 +1,13 @@
+/**
+ * The IDs of the users who have admin privileges.
+ */
 export const adminIds = ['382856633652477952']
+/**
+ * The IDs of the servers which bypass bot's normal restrictions.
+ * This is useful for testing or for servers that require special handling.
+ */
 export const serverIds = ['425670185089892362', '1018540510719258654']
+/**
+ * The channel ID for bot to send bug reports to
+ */
+export const bugReportChannelId = '1051145412142182511'

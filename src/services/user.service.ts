83186f1a8bfd800e18c3b9f78989ece189e602d0
Le Ngo Duc Manh <myt@nnsvn.me>
2025-07-10 00:54:50 +0700

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,12 +5,8 @@ import User, { UserDocument } from '@/models/user.model'
 /**
  * Finds a user by the given query. If no user is found and the query contains a userId,
  * a new user will be created with that userId.
- *
- * @param {FilterQuery<UserDocument>} query - The query to find the user.
- * @param {QueryOptions} [options={ lean: true }] - Options for the query, defaulting to lean mode.
- * @returns {Promise<UserDocument | null>} The found or created user document.
  */
-export async function getUser(query: FilterQuery<UserDocument>, options: QueryOptions = { lean: true }) {
+export async function getUser(query: FilterQuery<UserDocument>, options: QueryOptions = { lean: false }) {
   let user = await User.findOne(query, null, options)
 
   if (!user && query.userId) {

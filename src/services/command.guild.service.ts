dff093a8f04744af681d58d5fceea72d9f90df94
Le Ngo Duc Manh <myt@nnsvn.me>
2025-07-10 00:54:58 +0700

    Update command.guild.service.ts

--- src/services/command.guild.service.ts
+++ src/services/command.guild.service.ts
@@ -3,22 +3,17 @@ import { FilterQuery, QueryOptions } from 'mongoose'
 import CommandGuild, { CommandGuildDocument } from '@/models/command.guild.model'
 
 /**
- * Finds a guild by the given query. If no guild is found and the query contains a guildId,
- * a new guild will be created with that guildId.
- *
- * @param {FilterQuery<CommandGuildDocument>} query - The query to find the guild.
- * @param {QueryOptions} [options={ lean: true }] - Options for the query, defaulting to lean mode.
- * @returns {Promise<CommandGuildDocument | null>} The found or created guild document.
+ * Finds a guild command setting by the given query. If no guild is found and the query contains a guildId,
+ * a new document will be created with that guildId.
  */
 export async function getGuildCommandSettings(
   query: FilterQuery<CommandGuildDocument>,
-  options: QueryOptions = { lean: true }
+  options: QueryOptions = { lean: false }
 ) {
   let guild = await CommandGuild.findOne(query, null, options)
 
   if (!guild && query.guildId) {
     const defaultGuild: Partial<CommandGuildDocument> = {
-      _id: query.guildId, // Ensure _id is set to guildId
       guildId: query.guildId as string
     }
     guild = await CommandGuild.create(defaultGuild)

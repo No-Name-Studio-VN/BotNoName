d25eec6846301c7be73f2c1fcf0923638149ed37
Le Ngo Duc Manh <myt@nnsvn.me>
2025-07-10 00:54:52 +0700

    Update guild.service.ts

--- src/services/guild.service.ts
+++ src/services/guild.service.ts
@@ -5,12 +5,8 @@ import Guild, { GuildDocument } from '@/models/guild.model'
 /**
  * Finds a guild by the given query. If no guild is found and the query contains a guildId,
  * a new guild will be created with that guildId.
- *
- * @param {FilterQuery<GuildDocument>} query - The query to find the guild.
- * @param {QueryOptions} [options={ lean: true }] - Options for the query, defaulting to lean mode.
- * @returns {Promise<GuildDocument | null>} The found or created guild document.
  */
-export async function getGuild(query: FilterQuery<GuildDocument>, options: QueryOptions = { lean: true }) {
+export async function getGuild(query: FilterQuery<GuildDocument>, options: QueryOptions = { lean: false }) {
   let guild = await Guild.findOne(query, null, options)
 
   if (!guild && query.guildId) {
